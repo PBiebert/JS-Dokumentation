83ad7eaad3f961d92882b376c4917ac190b7d201
Philipp Biebert <philipp.biebert@gmail.com>
2025-09-11 16:01:02 +0200

    Difference between let and const and different data types

---
+++ basics.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//!Difference between let and const
+// 'let' variable can be changed
+let age = 27;
+console.log("Alter: ", age); // Output: 27
+
+age = 32;
+console.log("Neues Alter: ", age); // Output: 32
+
+// 'const' variable cannot be changed
+const birthYear = 1996;
+console.log("Geburtsjahr: ", birthYear); // Output: 1996
+
+//! Datatypes
+
+// String = text
+let myString = "Das ist ein Text";
+
+// Number = numeric value
+let myInt = 32; // integer
+
+let myFloat = 32.5466; // decimal number
+
+// Boolean = true or false
+let myBoolean = true;
+
+// Array = list of values
+let myArray = [2, 5, "Hallo"];
+
+// Object = key-value pairs
+let myObject = { age: 32, height: 177 };
